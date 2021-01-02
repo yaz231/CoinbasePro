121c39e6c30d1ee9913fd15620c1d71bf5ffc6b8
yaz231 <yazan_alatrach@hotmail.com>
2021-01-02 00:12:01 -0600

    Delete Keys.py

--- Keys.py
+++
@@ -1,8 +0,0 @@
-coinbase_API_key = '7CL24mQMZdkSc9KT'
-coinbase_API_passphrase = 'goaZlKXJWYIqu8XYDHj5M14RE66YK085'
-pro_coinbase_API_passphrase = 'fstucpg6z9u'
-b64secret = 'xErcUFTbZZTenafnRuWyZPB5cAMvOni5NTvz/YO3/zcnI56p66WEhuejtonNt/Y/24T8wRPtINRLvrnopfweug=='
-pro_coinbase_key = 'ec115e18af7633d64b086a2988226d3e'
-sandbox_secret = '5b9nw2236xg'
-sandbox_key = '01d8957c6b2243359e0fb6e31d18a935'
-sandbox_b64secret = 'mPaHSH7xZevvzu2KMEpDoYJGn3Yle/RFJSeodbh0adg0nlOVFhEjRHNH1o1xJZ1aw7yblbZwvL9/8Njb4VziYA=='
